b66d38920065305c5f99a0a73d44d7ee6ad9db44
howard <yeusun@gmail.com>
2023-01-23 22:01:34 +0900

    [yeusun] add the new file to display 'bye bye, Hello'

---
+++ SwiftUIAppForTutorial/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  SwiftUIAppForTutorial
+//
+//  Created by Lee HongWon on 2023/01/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye Bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
