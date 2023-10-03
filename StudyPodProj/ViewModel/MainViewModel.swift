35243ea9870cb57d75e6e5d033f1bfd15f87e5db
yeusun.lee <yeusun.lee@lge.com>
2023-10-04 00:07:56 +0900

    [iOS][LocalPackage] create the LocalPackage

--- StudyPodProj/ViewModel/MainViewModel.swift
+++ StudyPodProj/ViewModel/MainViewModel.swift
@@ -6,7 +6,13 @@
 //
 
 import Foundation
+import NetworkModule
 
 class MainViewModel {
-    
+    static let shared = MainViewModel()
+    private init() {}
+
+    func getNetworkModuleName() -> String {
+        return NetworkModule().text
+    }
 }
